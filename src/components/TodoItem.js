2fb09b6f73021e683f0c7ec04226444019c503f9
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-14 10:20:54 -0400

    Move cross icon to src and use by importing

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Draggable } from "react-beautiful-dnd";
+import deleteIcon from "../images/icon-cross.svg";
 
 const TodoItem = (props) => {
   const [isChecked, setIsChecked] = useState(props.itemIsChecked);
@@ -55,13 +56,7 @@ const TodoItem = (props) => {
           </div>
           <div className="delete-container">
             <button onClick={() => props.deleteFunction(props.itemID)}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-                <path
-                  fill="#494C6B"
-                  fillRule="evenodd"
-                  d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
-                />
-              </svg>
+              <img src={deleteIcon} alt="" />
             </button>
           </div>
         </div>
