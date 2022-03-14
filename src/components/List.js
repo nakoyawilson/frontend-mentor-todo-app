909609545b966117780fa8bd3f2c3b1737cdf4fc
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-14 18:49:22 -0400

    Fix margin issue

--- src/components/List.js
+++ src/components/List.js
@@ -21,7 +21,7 @@ const List = (props) => {
 
   return (
     <main className="list-container">
-      <div id="todo-list">
+      <div id="todo-list" class="todo-list">
         <Droppable droppableId="todo-list-1">
           {(provided) => (
             <div
